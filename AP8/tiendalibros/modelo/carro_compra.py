cb8aec345a58f77bb792bc70f3c81202c1236fa6
PabloGoez33 <luispablogoez@gmail.com>
2023-10-13 15:00:40 -0500

    AP8

---
+++ AP8/tiendalibros/modelo/carro_compra.py
@@ -0,0 +1,24 @@
+from modelo.item_compra import ItemCompra
+class CarroCompras:
+    # Defina metodo inicializador __init__(self)
+    def __init__(self):
+        self.item: list[ItemCompra] = []
+    
+    # Defina el metodo agregar_item
+    def agregar_item(self, libro, cantidad_libro):
+        nuevo_item: ItemCompra = ItemCompra(libro, cantidad_libro)
+        self.item.append(nuevo_item)
+        return nuevo_item
+
+    # Defina el metodo calcular_total
+    def calcular_total(self):
+        total = 0
+        for item in self.item:
+            total += item.calcular_subtotal()
+        return total
+    
+    # Defina el metodo quitar_item
+    def quitar_item(self, isbn):
+        self.item = [x for x in self.item if isbn != x.libro.isbn]
+        return
+    
---
+++ AP8/tiendalibros/modelo/tienda_libros.py
@@ -0,0 +1,11 @@
+from modelo.libro import Libro                                 
+class TiendaLibros:
+    # Defina metodo inicializador __init__
+    def __init__(self):
+        self.catalogo: dict = {}
+        self.carrito: 
+    # Defina metodo adicionar_libro_a_catalogo
+
+    # Defina metodo agregar_libro_a_carrito
+
+    # Defina metodo retirar_item_de_carrito
